8470c4d9b9133730741d11920f455ada8e954d0f
codelandcz <github@codeland.cz>
2014-11-28 21:53:45 +0100

    Marked  as unresolved

--- AMIFIB/src/Main.java
+++ AMIFIB/src/Main.java
@@ -15,11 +15,10 @@ public class Main
   static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
   static BufferedReader in  = new BufferedReader(new InputStreamReader(System.in));
 
+  //TODO Unresolved
   public static void main(String[] args) throws IOException
   {
     int N = Integer.parseInt(in.readLine());
-
-    int[]
 
     for (int i = 0; i < N; i++) {
       out.write(solve(in.readLine()));
